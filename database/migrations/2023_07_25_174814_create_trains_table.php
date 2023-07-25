9dfd39f25ac1a47218c3b3725069ae14d710e8cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 18:07:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- database/migrations/2023_07_25_174814_create_trains_table.php
+++ database/migrations/2023_07_25_174814_create_trains_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('train_name');
+            $table->string('departure_station');
+            $table->string('departure_time');
+            $table->string('_time')
             $table->timestamps();
         });
     }
