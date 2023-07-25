8dac2a8755792a99e2e500cca8c2c7c343181790
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 19:16:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/TrainStops.php
+++ app/Models/TrainStops.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrainStops extends Model
 {
+    protected $table = 'train_stops';
+    protected $primaryKey = 'id';
+    protected $fillable = ['train_id', 'station', 'arrival_time', 'departure_time', 'created_at', 'updated_at'];
     use HasFactory;
 }
