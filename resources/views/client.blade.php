f0b7acd2fddc690b1fc48b58814ce968a8255790
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 15:27:06 +0530

    Update

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -33,8 +33,10 @@
                 <div class="card text-bg-success mb-3" style="max-width: 18rem;">
                     <div class="card-header">My Tickets</div>
                     <div class="card-body">
-                      <h5 class="card-title">Success card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="row">
+                            <div class="col-lg-6"></div>
+                            <div class="col-lg-6"></div>
+                        </div>
                     </div>
                 </div>
             </div>
