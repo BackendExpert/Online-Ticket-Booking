cad5fed7d84192f09d67a3f520331f9638a143cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 18:19:19 +0530

    UPdaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $fillable = ['profile_img', 'name', 'email', 'role', 'address', 'created_at', 'updated_at'];
     use HasFactory;
 }
