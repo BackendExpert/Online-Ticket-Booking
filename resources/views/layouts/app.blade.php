f741f730e13071b105dc3627cd04280ac7fc7c55
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 14:54:25 +0530

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <i class="fas fa-shuttle-van"></i> {{__('Online Ticket Booking')}}
+                    <i class="fas fa-train"></i> {{__('Online Ticket Booking')}}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
