a2fa1c62c403d5d58a984f10945f20b6209e995c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 15:16:14 +0530

    Update

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -56,7 +56,15 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3"></div>
+            <div class="col-lg-3">
+                <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                      <h5 class="card-title">Warning card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
