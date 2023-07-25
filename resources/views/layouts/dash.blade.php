21fe3b54e0c0a78d37193fd41fd6011c570b5d25
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 15:04:27 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -103,21 +103,8 @@
         <div class="list-group list-group-flush my-3">
             <a href="{{url('/admin')}}" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                     class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
-
-            @php
-                if(Auth::user()->role == 1 && Auth::user()->role == 2){
-            @endphp
-                <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+            <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
                     class="fas fa-user-tie me-2"></i>All Users</a>
-            @php
-                }elseif (Auth::user()->role == 3) {
-            @endphp 
-                <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                    class="fas fa-user-tie me-2"></i>All Users</a>
-            @php
-                }
-            @endphp
-
 
 
             
