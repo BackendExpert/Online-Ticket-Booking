d71e93bb144f68b1aff24888b201264a50c0e7b1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 15:49:02 +0530

    Update

--- database/migrations/2023_07_25_154644_create_my_bookings_table.php
+++ database/migrations/2023_07_25_154644_create_my_bookings_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('my_bookings', function (Blueprint $table) {
             $table->id();
+            $table->string('booker');
             $table->timestamps();
         });
     }
