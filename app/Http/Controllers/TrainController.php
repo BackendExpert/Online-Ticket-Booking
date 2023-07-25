b16b102fb19b87c0bc6c381ba8b4d9de423b7739
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 20:31:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -53,7 +53,7 @@ class TrainController extends Controller
             'departure_time' => $request->input('departure_time'),
             'arrival_station' => $request->input('arrival_station'),
             'arrival_time' => $request->input('arrival_time'),
-            'weekly_schedule' => $request->input('weekly_schedule'),
+            'weekly_schedule' => $request->,
             'stops' => $request->input('stops'),
             'is_active' => $request->input('is_active'),
         ]);
