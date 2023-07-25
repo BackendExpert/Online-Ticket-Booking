9c4e0e0c7fa8c8f3d80ea3fff7f7e281a494472d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 20:07:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/Trains/index.blade.php
+++ resources/views/Trains/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="admin-card">
         <h2>Trains</h2>
-
-        <a href="{{ url('Trains/create') }}"><button class="btn btn-success">Add Train</button></a>
+        <hr><br><br>
+        <a href="{{ url('Trains/create') }}"><button class="btn btn-success"><i class="fas fa-train"></i> Add Train</button></a>
     </div>
 @endsection
