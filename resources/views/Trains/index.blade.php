d5661b7da00731688947eb4d6850f51f3146edb4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 19:40:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/Trains/index.blade.php
+++ resources/views/Trains/index.blade.php
@@ -3,5 +3,7 @@
 @section('content')
     <div class="admin-card">
         All Trains
+
+        <a href="{{ url('Trains/create') }}"><button class="btn btn-success">Add Train</button></a>
     </div>
 @endsection
