eac52015457f29c1b99ec03aec923310c931ebca
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 15:14:32 +0530

    Update

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -28,7 +28,21 @@
         {{-- User Name : <b>@php  echo auth()->user()->name;   @endphp</b> <br>
         User Email : <b>@php  echo auth()->user()->email;   @endphp</b> --}}
 
-        
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                      <h5 class="card-title">Success card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3"></div>
+            <div class="col-lg-3"></div>
+            <div class="col-lg-3"></div>
+        </div>
+
     </div>
 
 @endsection
