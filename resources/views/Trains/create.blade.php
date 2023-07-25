7bfb7b0e7c45a463f36f7593c39dec42aa396302
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 19:40:54 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ resources/views/Trains/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dash')
+
+@section('content')
+    <div class="admin-card">
+        All Trains
+
+        <a href="{{ url('Trains') }}"><button class="btn btn-primary">Back</button></a>
+    </div>
+@endsection
