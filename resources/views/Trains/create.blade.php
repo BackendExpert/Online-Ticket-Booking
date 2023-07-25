8a93c39c7e1b7b44d3f5696b9655681b6f0f46d8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 19:44:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/Trains/create.blade.php
+++ resources/views/Trains/create.blade.php
@@ -23,11 +23,17 @@
             <label for="Train Name">arrival_station</label><br>
             <input type="text" name="arrival_station" id="" class="form-control">
 
-            <label for="Train Name">Train Name</label><br>
-            <input type="text" name="train_name" id="" class="form-control">
+            <label for="Train Name">arrival_time</label><br>
+            <input type="text" name="arrival_time" id="" class="form-control">
 
-            <label for="Train Name">Train Name</label><br>
-            <input type="text" name="train_name" id="" class="form-control">
+            <label for="Train Name">weekly_schedule</label><br>
+            <input type="text" name="weekly_schedule" id="" class="form-control">
+
+            <label for="Train Name">weekly_schedule</label><br>
+            <input type="text" name="weekly_schedule" id="" class="form-control">
+
+            <label for="Train Name">weekly_schedule</label><br>
+            <input type="text" name="weekly_schedule" id="" class="form-control">
         </form>
     </div>
 @endsection
