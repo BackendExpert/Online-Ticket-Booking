bc2a84113211b79c484bea03264020819194bbd7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 15:15:16 +0530

    Update

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -47,7 +47,15 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3"></div>
+            <div class="col-lg-3">
+                <div class="card text-bg-info mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                      <h5 class="card-title">Info card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
             <div class="col-lg-3"></div>
         </div>
 
