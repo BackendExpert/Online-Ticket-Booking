4de53871a0abb2772bcdd6b24a239f0b9164a013
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 19:43:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/Trains/create.blade.php
+++ resources/views/Trains/create.blade.php
@@ -11,7 +11,23 @@
         <form action="{{ url('Trains') }}" method="POST">
             @csrf
 
-            
+            <label for="Train Name">Train Name</label><br>
+            <input type="text" name="train_name" id="" class="form-control">
+
+            <label for="Train Name">departure_station</label><br>
+            <input type="text" name="departure_station" id="" class="form-control">
+
+            <label for="Train Name">departure_time</label><br>
+            <input type="text" name="departure_time" id="" class="form-control">
+
+            <label for="Train Name">arrival_station</label><br>
+            <input type="text" name="arrival_station" id="" class="form-control">
+
+            <label for="Train Name">Train Name</label><br>
+            <input type="text" name="train_name" id="" class="form-control">
+
+            <label for="Train Name">Train Name</label><br>
+            <input type="text" name="train_name" id="" class="form-control">
         </form>
     </div>
 @endsection
