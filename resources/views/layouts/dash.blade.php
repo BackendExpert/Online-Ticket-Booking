4f58c53c3d02417fb0040b27ba48a3ef105b78f2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-25 14:59:38 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -104,7 +104,11 @@
             <a href="{{url('/admin')}}" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                     class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
 
-                    
+            @if (Auth::user()->role == 1 && Auth::user()->role == 2)
+                
+            @else
+                
+            @endif        
             <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
                     class="fas fa-user-tie me-2"></i>All Users</a>
             
