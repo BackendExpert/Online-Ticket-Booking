6e7cd9899264e759c6089e418a133f71e974355c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-02 21:23:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/Trains/create.blade.php
+++ resources/views/Trains/create.blade.php
@@ -144,10 +144,10 @@
                     
                     <div class="row">
                         <div class="col-lg-3">
-                            <input type="text" name="station[]" placeholder="Enter data" class="form-control">
+                            <input type="text" name="station[]" placeholder="Station Name" class="form-control">
                         </div>
                         <div class="col-lg-3">
-                            <input type="time" name="time_arrival[]" placeholder="Enter data" class="form-control">
+                            <input type="time" name="time_departure[]" placeholder="departure" class="form-control">
                         </div>
                         <div class="col-lg-3">
                             <input type="time" name="time_departure[]" placeholder="Enter data" class="form-control">
