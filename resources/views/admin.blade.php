5a21efad6508929005bea95cbefc8e2cce1dbf31
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-03 16:46:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -48,68 +48,73 @@
             </div>
         </div> --}}
 
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="card text-bg-success mb-3" style="max-width: 18rem;">
-                    <div class="card-header">My Tickets</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <p style="font-size: 50px;"><i class="fa fa-ticket"></i></p>
-                            </div>
-                            <div class="col-lg-6">
-                                <p style="font-size: 25px;">50</i></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
-                    <div class="card-header">Last Re-Charge</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <p style="font-size: 50px;"><i class="fas fa-dollar-sign"></i></p>
-                            </div>
-                            <div class="col-lg-6">
-                                <p style="font-size: 25px;">1500.00</i></p>
+        <div class="admin-card">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+                        <div class="card-header">My Tickets</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <p style="font-size: 50px;"><i class="fa fa-ticket"></i></p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <p style="font-size: 25px;">50</i></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card text-bg-info mb-3" style="max-width: 18rem;">
-                    <div class="card-header">Payments</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <p style="font-size: 50px;"><i class="fas fa-money-bill-wave"></i></p>
-                            </div>
-                            <div class="col-lg-6">
-                                <p style="font-size: 25px;">50</i></p>
+                <div class="col-lg-3">
+                    <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Last Re-Charge</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <p style="font-size: 50px;"><i class="fas fa-dollar-sign"></i></p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <p style="font-size: 25px;">1500.00</i></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
-                    <div class="card-header">My Balance</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <p style="font-size: 50px;"><i class="fas fa-dollar-sign"></i></p>
+                <div class="col-lg-3">
+                    <div class="card text-bg-info mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Payments</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <p style="font-size: 50px;"><i class="fas fa-money-bill-wave"></i></p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <p style="font-size: 25px;">50</i></p>
+                                </div>
                             </div>
-                            <div class="col-lg-6">
-                                <p style="font-size: 25px;">5000.00</i></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+                        <div class="card-header">My Balance</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <p style="font-size: 50px;"><i class="fas fa-dollar-sign"></i></p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <p style="font-size: 25px;">5000.00</i></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
+      
 
 
 
