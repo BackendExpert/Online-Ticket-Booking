30f9c91f90f74fa259e04796b4163f5e83fa3379
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 19:45:02 +0530

    Update

--- resources/views/Usersettings/settings.blade.php
+++ resources/views/Usersettings/settings.blade.php
@@ -8,5 +8,16 @@
     <div class="admin-card">
         <h3><i class="fa fa-gears"></i> Account Settings</h3>
         <hr><br>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h5>Name : </h5>
+            </div>
+            <div class="col-lg-6">
+                <h6> {{ $member_setting->name }} </h6>
+            </div>
+        </div>
+
+        
     </div>
 @endsection
