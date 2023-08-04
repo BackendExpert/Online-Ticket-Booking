4dc850633c14a01c6e6091d2268b058a7284a168
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 18:38:10 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\String_;
 
 class UsersettingController extends Controller
 {
-    //
+    public function account_settings(String $id){
+        
+    }
 }
