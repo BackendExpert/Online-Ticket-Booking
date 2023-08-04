d9f12bf17383fad7d65c0dd1d43ff6f81c496b44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 19:40:03 +0530

    Update

--- resources/views/Usersettings/settings.blade.php
+++ resources/views/Usersettings/settings.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dash')
 
 @if (Auth::user()->id != $member_setting->id)
-<script>window.location = "/client";</script>
+<script>window.location = "/";</script>
 @endif
 
 @section('content')
