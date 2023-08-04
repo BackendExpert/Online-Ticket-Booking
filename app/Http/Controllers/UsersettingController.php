fd3e890fda84c4d85194de8b58b4994226803dbb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 09:49:37 +0530

    Update

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -2,65 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Member;
 use Illuminate\Http\Request;
 
 class UsersettingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $member_settings = Member::find($id);
-        return view('Usersettings.edit')->with('member_settings', $member_settings);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    //
 }
