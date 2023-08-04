b0ae0c7e18a9c3c0166dd693f6e2fe6eb517db85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 18:38:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\Cast\String_;
 
 class UsersettingController extends Controller
 {
     public function account_settings(String $id){
-        
+        $member_setting = Member::find($id);
+        return view('')
     }
 }
