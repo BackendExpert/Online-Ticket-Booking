4846e1bd600812b955205c2b40b1223f32eba985
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 19:36:19 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -52,4 +52,4 @@ Route::resource('/Trains', TrainController::class);
 
 Route::get('/ViewTrain', [ViewTrainController::class, 'viewAllTrains'])->name('ViewTrain');
 
-Route::get('/Usersettings/'. Auth::user()->id .'/settings', [UsersettingController::class, 'account_settings']);
+Route::get('/Usersettings/{id}/settings', [UsersettingController::class, 'account_settings']);
