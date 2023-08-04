303bd748c2797cbc416f1921721d7369d0d1b137
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-04 18:39:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -10,6 +10,6 @@ class UsersettingController extends Controller
 {
     public function account_settings(String $id){
         $member_setting = Member::find($id);
-        return view('Usersettings.settings')
+        return view('Usersettings.settings')->with('member_setting', $member_setting);
     }
 }
