5853038eb4a0e33945c26f5941e0cd1ed5b8ee85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-06 08:54:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/password/edit.blade.php
+++ resources/views/password/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="admin-card">
         <h3><i class="fas fa-key"></i> Password Reset</h3>
         <hr>
-        <a href=""><button class="btn btn-primary">Back</button></a><br>
+        <a href="{{ url('/Usersettings/'.Auth::user()->id.'/settings'); }}"><button class="btn btn-primary">Back</button></a><br>
         
     </div>
 @endsection
