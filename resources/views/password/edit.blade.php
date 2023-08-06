08b8f312dd346b7ed84f90487a8eb4ad023b943c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-06 09:05:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/password/edit.blade.php
+++ resources/views/password/edit.blade.php
@@ -14,7 +14,9 @@
             @csrf
 
             <div class="row">
-                <div class="col-lg-2"></div>
+                <div class="col-lg-2">
+                    <label for="current_pass">Current Password : </label>
+                </div>
                 <div class="col-lg-10"></div>
             </div>
             
